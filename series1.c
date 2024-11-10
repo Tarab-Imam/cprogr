21fcf3e242ce31eab62d63ab521218e3d949deff
Tarab Imam <imamtarab@gmail.com>
2024-11-11 00:33:49 +0530

    dfg

---
+++ series1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int n=0;
+    int sum=0;
+    for (i=1;i<=3;i++){
+    n=n*10+9;
+    sum=sum+n;
+    }
+    printf("%d",sum);    
+    return 0;
+
+
+}
